db2329f48a362867bc99c1f625cfed40ebf34a44
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-08-19 14:09:24 +0600

    featured and latest news section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins:['"Poppins"', "sans-serif"]
+      },
+      grayscale: {
+        100: '100%',
+      }
+    },
   },
   plugins: [],
 }
